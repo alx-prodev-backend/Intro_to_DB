e12c15c189606c45627c7899a04c2dd62058e745
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-09 02:40:32 +0200

    Task 00 0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+# Addition of 10 and 5 is 15
+number1 = 10
+number2 = 5
+
+print(f"Addition of 10 and 5 is {number1+number2}")
+print(f"Subtraction of 10 and 5 is {number1-number2}")
+print(f"AMultiplication of 10 and 5 is {number1*number2}")
