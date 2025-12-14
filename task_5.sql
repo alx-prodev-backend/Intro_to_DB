8435cda46de7818e71a4c0f47429d1768b76fcfe
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2025-12-14 23:52:20 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- Task 5: Insert a single customer
 -- File: task_5.sql
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
+SELECT * FROM customers;
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
